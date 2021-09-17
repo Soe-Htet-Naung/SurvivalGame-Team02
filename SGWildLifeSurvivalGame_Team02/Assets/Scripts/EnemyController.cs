1b054eeb3906c4f073c0428623d3fb3f310063ab
SaltyHunter <saltyhunters888@gmail.com>
2021-09-17 15:11:41 +0700

    add enemy manager script

---
+++ SGWildLifeSurvivalGame_Team02/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+    
+
+    public class EnemyController : MonoBehaviour
+    {
+        bool isPerformingAction;
+
+        [Header("AI Settings")]
+
+        public float detectionRadius;
+
+        // Awake Method
+        private void Awake()
+        {
+
+        }
+        // Update is called once per frame
+        private void Update()
+        {
+            
+        }
+
+        private void HandleCurrentAction()
+        {
+
+        }
+
+    }
+}
