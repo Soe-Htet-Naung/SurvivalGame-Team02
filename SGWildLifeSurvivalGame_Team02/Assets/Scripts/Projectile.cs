a0f041944f751190b9e74203ad34c0ea04a2daff
SaltyHunter <saltyhunters888@gmail.com>
2021-09-25 21:03:11 +0700

    Update Projectile.cs

--- SGWildLifeSurvivalGame_Team02/Assets/Scripts/Projectile.cs
+++ SGWildLifeSurvivalGame_Team02/Assets/Scripts/Projectile.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed;
-    public UnityEngine.AI.NavMeshAgent projectile;
+    private Vector2 target;
     public Transform Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(Player.position.x, Player.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+       if (transform.position.x == target.x && transform.position.y == target.y){
+           DestroyProjectile();
+       } 
+    }
+    void OnTriggerEnter3d(SphereCollider other){
+        if (other.CompareTag("Player")){
+
+        }
+    }
+    void DestroyProjectile(){
+        Destroy(gameObject);
     }
 }
+
