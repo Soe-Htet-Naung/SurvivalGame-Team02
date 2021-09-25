00f1bca4d5f67dcf4634b03b18a262b42f49d853
SaltyHunter <saltyhunters888@gmail.com>
2021-09-25 17:28:03 +0700

    creted the most cursed creature in the universe

--- SGWildLifeSurvivalGame_Team02/Assets/Scripts/RangedAI.cs
+++ SGWildLifeSurvivalGame_Team02/Assets/Scripts/RangedAI.cs
@@ -8,34 +8,53 @@ public class RangedAI : MonoBehaviour{
     public float lookRadius = 20f;
     public float attackRadius = 10f;
 
+    private float timeBtwShots;
+    
+    public float startTimeBtwShots;
+
+    public GameObject Projectile;
+
     public NavMeshAgent enemy;
 
     public Transform Player;
 
+    public bool playerInSightRange;
+    public bool playerInAttackRange;
 
+    
+    bool allreadyAttacked;
     public Transform target;
     NavMeshAgent agent;
 
-
+   
     // Start is called before the first frame update
     void Start()
     {
         target = Player;
         agent = GetComponent<NavMeshAgent>();
+        timeBtwShots = startTimeBtwShots;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Check for player in sight range 
+
         float distance = Vector3.Distance(target.position,transform.position);
 
         if (distance<= lookRadius)
         {
             enemy.SetDestination(Player.position);
 
-            if (distance<= agent.stoppingDistance)
+            if (distance<= agent.stoppingDistance || timeBtwShots <= 0)
             {
                 //attack the target with projectiles
+                Instantiate(Projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
             }
         }
     }
